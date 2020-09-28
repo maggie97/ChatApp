357b672063713c01435f5b0465e903ef564f2991
maggie97 <maggielee97@icloud.com>
2020-09-28 18:39:20 -0500

    Initial Commit

---
+++ Contacts List with Real Time Chat Application/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Contacts List with Real Time Chat Application
+//
+//  Created by maggie mendez on 28/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Contacts List with Real Time Chat ApplicationTests/Contacts_List_with_Real_Time_Chat_ApplicationTests.swift
@@ -0,0 +1,33 @@
+//
+//  Contacts_List_with_Real_Time_Chat_ApplicationTests.swift
+//  Contacts List with Real Time Chat ApplicationTests
+//
+//  Created by maggie mendez on 28/09/20.
+//
+
+import XCTest
+@testable import Contacts_List_with_Real_Time_Chat_Application
+
+class Contacts_List_with_Real_Time_Chat_ApplicationTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
